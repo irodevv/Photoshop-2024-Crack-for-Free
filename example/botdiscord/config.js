a027de65b29299101f5a61537e3daa645f0dc5b7
zougataga <107312482+zougataga@users.noreply.github.com>
2023-03-15 16:52:48 +0100

    Add files via upload

---
+++ example/botdiscord/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    token: "MTA4NDc4ODAwODY1NTUzNjE0OA.GMLm3k.O5qKLgS4yXMP_u2Gjpryo_4K6ZGWjehFZi9DoA",
+
+    moniteur: {
+        interval: 5000,
+        debug: false,
+        urls: [
+            // { url: "url vinted", salon: "salon logs" }
+            { url: "https://www.vinted.fr/vetements?search_text=bonnet&order=newest_first", salon: "1084788444057849976" }
+        ]
+    }
+}
